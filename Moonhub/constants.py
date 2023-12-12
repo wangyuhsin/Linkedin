0dde553a9b8803743f2d2e967945d3b554a5f2fa
Yuhsin Wang <76431031+wangyuhsin@users.noreply.github.com>
2023-12-12 01:07:03 -0800

    Delete Moonhub/constants.py

--- Moonhub/constants.py
+++
@@ -1,7 +0,0 @@
-# LinkedIn cookies
-# Check https://help.delpha.io/s/article/How-to-access-your-LinkedIn-Cookie to learn how to get your cookies
-LI_AT = "AQEDASQhyy..."
-JSESSIONID = "ajax:683..."
-
-SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1gOWPrjPQ84INwZF_Au-Oif_ztdG36SBsuRmmxDsrFXA/edit#gid=0"
-SHEET_NAME = "Sheet1"
